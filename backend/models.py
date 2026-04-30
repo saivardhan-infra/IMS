5611affb58776bb866b3752f8b30822814323c06
Saivardhan <svardhankarri@gmail.com>
2026-04-30 14:53:01 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,11 @@
+from pydantic import BaseModel
+from datetime import datetime
 
+class Signal(BaseModel):
+    component_id: str
+    message: str
+    timestamp: datetime
+
+class RCA(BaseModel):
+    cause: str
+    fix: str
