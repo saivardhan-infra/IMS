4e5def5701a2527cfc86636d47587de05cbf562e
Saivardhan <svardhankarri@gmail.com>
2026-04-30 14:56:48 +0530

    Update simulate_outage.sh

--- scripts/simulate_outage.sh
+++ scripts/simulate_outage.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+for i in {1..500}
+do
+  curl -X POST http://localhost:8000/signals \
+  -H "Content-Type: application/json" \
+  -d '{"component_id":"DB","message":"failure","timestamp":"2026-01-01T10:00:00"}' &
+done
+
+wait
+echo "Done"
